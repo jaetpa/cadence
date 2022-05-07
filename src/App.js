fa13ae7de11a74050c65f1cbe6db1c2abf550f77
Jarone <jaetpa@gmail.com>
2022-05-07 01:57:05 +0100

    Built initial grid layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <div className="navigation">
+          <h1 className="app-logo">
+            <a href="index.html">Cadence</a>
+          </h1>
+          <input type="search" className="search-bar"></input>
+          <div className="login-signup-buttons">
+            <button className="nav-button">Sign up</button>
+            <button className="nav-button">Log in</button>
+          </div>
+        </div>
+      </nav>
+      <div className="content-container">
+        <div className="content-grid">
+          <div className="grid-box"></div>
+          <div className="grid-box"></div>
+          <div className="grid-box"></div>
+          <div className="grid-box"></div>
+        </div>
+      </div>
     </div>
   );
 }
