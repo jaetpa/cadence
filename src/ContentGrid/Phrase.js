51385c6e4d9358b56c46aba7730432502208f938
jarone-engine <jarone@enginecreative.co.uk>
2022-07-09 16:19:46 +0100

    Add Phrase and Player components

---
+++ src/ContentGrid/Phrase.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Phrase = () => {
+    return ( 
+        <div className="text-5xl font-bold text-blue-700 hover:text-blue-400 text-left"><a href="">phrase</a></div>
+     );
+}
+ 
+export default Phrase;
